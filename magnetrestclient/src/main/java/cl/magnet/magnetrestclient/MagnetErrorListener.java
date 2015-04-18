d3a220749c047a279e94dea507024cbe793beb1d
Lukas Zorich <lukas.zorich@gmail.com>
2015-04-18 12:40:15 -0300

    Documentation of MagnetErrorListener

--- magnetrestclient/src/main/java/cl/magnet/magnetrestclient/MagnetErrorListener.java
+++ magnetrestclient/src/main/java/cl/magnet/magnetrestclient/MagnetErrorListener.java
@@ -6,9 +6,11 @@ import com.android.volley.VolleyError;
 
 /**
  * Abstract class that divides error responses in three:
- * - Unauthorized errors: 401
- * - upgrade required error: 426
- * - other errors
+ * <ul>
+ *     <li>Unauthorized error: response status code 401</li>
+ *     <li>Upgrade required error: response status code 426</li>
+ *     <li>Other errors</li>
+ * </ul>
  *
  * Created by lukas on 18-03-15.
  */
@@ -31,9 +33,33 @@ public abstract class MagnetErrorListener implements Response.ErrorListener {
         }
     }
 
+    /**
+     * Method called when an unhandled error has been ocurred. This happens when the server
+     * responds with a 4xx or 5xx status code, with the exception of 401 and 426 codes,
+     * that are handled by {@link #onUnauthorizedError(com.android.volley.VolleyError, com.android.volley.Request)}
+     * and {@link #onUpgradeRequiredError(com.android.volley.VolleyError)} respectively.
+     *
+     * @param volleyError The error with the provided error code.
+     */
     public abstract void onUnhandledError(VolleyError volleyError);
 
+    /**
+     * Method called when an Unauthorized error has been ocurred. This happens when the
+     * server responds with an 401 status code.
+     *
+     * @see <a href="http://tools.ietf.org/html/rfc2616#section-10.4.2">401 Unauthorized</a>
+     *
+     * @param volleyError The error with the provided error code.
+     * @param request The request that triggered the error.
+     * @param <T> Parsed type of the request.
+     */
     public abstract <T> void onUnauthorizedError(VolleyError volleyError, final Request<T> request);
 
+    /**
+     * Method called when an Upgrade Required error has been ocurred. This happens when the
+     * server responds with a 426 status code.
+     *
+     * @param volleyError The error with the provided error code.
+     */
     public abstract void onUpgradeRequiredError(VolleyError volleyError);
 }
